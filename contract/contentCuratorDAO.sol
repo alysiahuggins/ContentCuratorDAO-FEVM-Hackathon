13240188f7647d34ebaacf793140c0cd2cc29da6
Alysia Huggins <hi@alysia.tech>
2022-11-19 21:19:42 -0800

    progress so far - voting works

---
+++ contract/contentCuratorDAO.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import "./ContentCuratorDAOToken.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract ContentCuratorDAO {
+    struct Post{
+        uint256 id;
+        bool exists;
+        uint256 numVotes;
+        address [] voters;
+        bool votePassed;
+        string postIPFSURL;
+        string postOwner;
+    }
+
+    uint256 numPostsWithVotes;
+    address daoTokenAddress;
+    address owner;
+    mapping(string => Post) private posts;
+    mapping(address => string[]) private daoMemberVotes;
+    mapping(string => uint256) private postVotes;
+
+    constructor(address _daoTokenAddress){
+        daoTokenAddress = _daoTokenAddress;
+        owner = msg.sender;
+        numPostsWithVotes = 0;
+    }
+
+    function mintDAOToken(address user)
+    external 
+    {
+        ContentCuratorDAOToken daoToken = ContentCuratorDAOToken(daoTokenAddress);
+        require(daoToken.balanceOf(user)==0, "You already received free tokens, you have to buy more tokens to vote.");
+        daoToken.mint(user, 5 ether);
+    }
+
+    function mintDAOToken()
+    external 
+    {
+        address user = msg.sender;
+        ContentCuratorDAOToken daoToken = ContentCuratorDAOToken(daoTokenAddress);
+        require(daoToken.balanceOf(user)==0, "You already received free tokens, you have to buy more tokens to vote.");
+        daoToken.mint(user, 5 ether);
+    }
+    
+
+    function hasDAOToken(address user)
+    external
+    view
+    returns (bool)
+        {
+            ERC20 token = ERC20(daoTokenAddress);
+            uint256 tokenBalance = token.balanceOf(user);
+            if(tokenBalance>0) return true;
+            return false;
+    }
+
+    modifier onlyDAOMember(string memory message) {
+        require(this.hasDAOToken(msg.sender), message);
+        _;
+    }
+
+    function createPost(
+        string memory _postIPFSURL,
+        string memory _postOwner,
+        address _voter
+    )
+        internal
+        returns ( Post storage )
+    {
+        uint256 postId = numPostsWithVotes++;
+        Post storage post = posts[_postOwner];
+        post.exists = true;
+        post.id = postId;
+        post.numVotes = 1;
+        post.votePassed = false;
+        post.voters = [_voter];
+        post.postIPFSURL = _postIPFSURL;
+        post.postOwner = _postOwner;
+        return post;
+
+    }
+
+    function vote(string memory _postIPFSURL, string memory _postOwner)
+        external
+        onlyDAOMember("Only DAO Members are allowed to vote")
+    {
+        Post storage post = posts[_postIPFSURL];
+        if(!post.exists){
+            post = createPost(_postIPFSURL, _postOwner, msg.sender);
+            posts[_postIPFSURL] = post;
+        }else{
+            post.numVotes++;
+            post.voters.push(msg.sender);
+        }
+        postVotes[_postIPFSURL] = post.numVotes;
+        daoMemberVotes[msg.sender].push(_postIPFSURL);
+
+    }
+
+    
+    
+
+    function getDaoMemberVotes()
+    external
+    view
+    returns ( string[] memory)
+    {
+        return daoMemberVotes[msg.sender];
+    }
+
+
+    function getPosts(string memory postIPFSURL )
+    external
+    view
+    returns (Post memory){
+        return posts[postIPFSURL];
+    }
+
+    function getNumPostsWithVotes()
+    external
+    view
+    returns (uint256 postsWithVotes){
+        return numPostsWithVotes;
+    }
+
+    function getPostVoters(string memory postIPFSURL )
+    external
+    view
+    returns (address [] memory){
+        return posts[postIPFSURL].voters;
+    }
+
+    function getPostVotes(string memory postIPFSURL )
+    external
+    view
+    returns (uint256){
+        return postVotes[postIPFSURL];
+    }
+
+
+
+    
+}
---
+++ src/main.js
@@ -0,0 +1,388 @@
+import Web3 from 'web3'
+// import { newKitFromWeb3 } from '@celo/contractkit'
+import BigNumber from "bignumber.js"
+import erc20Abi from '../contract/erc20.abi.json'
+import contentCuratorDAOAbi from '../contract/contentCuratorDAO.abi.json'
+import contentCuratorDAOTokenAbi from '../contract/contentCuratorDAOToken.abi.json'
+
+
+
+/** TODOg
+ * update shares button
+ * show how many shares the user has
+ */
+
+const ERC20_DECIMALS = 18
+
+let kit
+const MPContractAddress = "0x634cED1A66D18744D44716A741284118B687b061"
+const cUSDTokenAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+const contentCuratorDAOAddress = "0x66880f93c6a64bed43415D4F01043Bf953D7D726"
+const contentCuratorDAOTokenAddress = "0xF188BC880974fcbF9C323D4385060e7E9469CA8a"
+let contract
+let erc20Contract
+let properties = []
+let web3 
+let defaultAccount
+let filecoinTestnetID=31415
+let celoExplorer = "https://alfajores-blockscout.celo-testnet.org/"
+let bgColor = "#edc0e0"
+let walletConnected = false;
+
+// ["Best House","https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2021/08/download-23.jpg","Best house on the block","London, UK"]
+// [300000000000000000000000,0,1000000]
+
+
+ethereum.on('chainChanged', (_chainId) => window.location.reload());
+
+const connectMetamaskWallet = async function () {
+    if (window.ethereum) {
+        notification("⚠️ Please approve this DApp to use it.")
+        try{
+            const chainId = await ethereum.request({ method: 'eth_chainId' });
+            if(parseInt(chainId,16)!=filecoinTestnetID){
+                throw "⚠️ Please switch to the Filecoin Wallaby Testnet to use this app."
+            }
+            
+            web3 = new Web3(window.ethereum);
+
+            await window.ethereum.enable();
+            notificationOff()
+
+            const accounts = await web3.eth.getAccounts()
+            defaultAccount = accounts[0];    
+            console.log(`accounts ${accounts}`)
+
+            
+
+        } catch (error) {
+            notification(`⚠️ ${error}.`)
+          }
+        
+      }else {
+        notification("⚠️ Please install the Metamask.")
+      }
+  }
+
+  
+  let posts = [
+    {
+      image: "https://pbs.twimg.com/media/FhoePCRUAAA1WLG?format=jpg&name=large",
+      description: "Welcoming back Ligma & Johnson!",
+      owner: "Elon Musk",
+      numVotes: 0,
+      status: 0,
+      ipfsURL: "ipfs://1"
+    },
+    {
+      image: "https://pbs.twimg.com/media/FhS6MXBUUAYC6y3?format=jpg&name=large",
+      description: "Twitter HQ is great (this is a real pic)",
+      owner: "Elon Musk",
+      numVotes: 0,
+      status: 0,
+      ipfsURL: "ipfs://2",
+    }
+  ]
+  
+
+  
+
+  const getBalance = async function () {
+    web3 = new Web3(window.ethereum);
+
+    let balance = await web3.eth.getBalance(defaultAccount);
+    console.log(balance);
+    document.querySelector("#balance").textContent = parseFloat(web3.utils.fromWei(balance, 'ether')).toFixed(2)
+  }
+
+  const getDAOTokenBalance = async function () {
+    web3 = new Web3(window.ethereum);
+
+    let balance = await erc20Contract.methods.balanceOf(defaultAccount).call();
+    console.log(balance);
+    document.querySelector("#balance").textContent = parseFloat(web3.utils.fromWei(balance, 'ether')).toFixed(2)
+  }
+
+
+
+  async function renderPosts() {
+    console.log(`render posts ${posts.length}`)
+    console.log(`get Post Votes`)
+
+    document.getElementById("marketplace").innerHTML = ""
+    posts.forEach(async (_post, index) => {
+      console.log(_post)
+      const _postVotes = await contract.methods.getPostVotes(_post.ipfsURL).call();
+      _post.numVotes = _postVotes;
+      posts[index] = _post;
+      const newDiv = document.createElement("div")
+      newDiv.className = "col-md-4"
+      newDiv.innerHTML = postsTemplate(_post, index)
+
+      document.getElementById("marketplace").appendChild(newDiv)
+    })
+  }
+
+
+  function postsTemplate(_post, index) {
+    let viewerIsOwner = _post.owner==defaultAccount
+    
+      return `
+      <div class="card mb-4">
+        <img class="card-img-top img-thumbnail" src="${_post.image}" alt="...">
+        
+        <h4 class="position-absolute top-0 end-0 mt-5 px-2 py-1 rounded-start" style="background-color: ${bgColor};">
+          ${_post.numVotes} Votes
+        </h4>
+        <!--div class="card-body text-left p-4 position-relative">
+        <div class="translate-middle-y position-absolute top-0">
+        ${_post.owner}
+        </div>
+        </div-->
+        <h2 class="card-title fs-4 fw-bold mt-2">${_post.owner}</h2>
+        <p class="card-text mb-4" style="min-height: 82px">
+          ${_post.description}             
+        </p>
+        
+        
+        <p class="card-text mt-4 ">
+          <i class="bi bi-megaphone-fill" ></i>
+          <span>${_post.status==0?'More Votes Needed':'Accepted'}</span>
+        </p>
+       
+        <div class="d-grid gap-2">
+          <!-- do not show buy button if property status is not 0 (which means on sale)-->
+          <a class="btn btn-lg btn-outline-dark buyBtn voteBtn fs-6 p-3" style="${_post.status==0?'display:block':'display:none'}" id=${
+            index
+          }>
+            Vote
+          </a>
+
+
+          <!-- only show update price and cancel buttons if current viewer is the owner and there are no properties sold-->
+          <a class="btn btn-lg btn-outline-dark updatePriceBtn fs-6 p-3" style="${_post.status!=0||viewerIsOwner!=true||_post.sold>0?'display:none':'display:block'}" id=${index
+          }
+              data-bs-toggle="modal"
+              data-bs-target="#updatePriceModal-${
+                index}">
+            Update Property
+          </a>
+          
+          <!--a class="btn btn-lg btn-outline-dark houseTokenBtn fs-6 p-3" href= "${celoExplorer}token/${_post.houseTokenAddress}/token-transfers" target="_blank" id=${
+            index} style="${_post.status!=0?'display:none':'display:block'}"
+          >
+            Buy DAO Token to Vote
+          </a-->
+        </div>
+      </div>
+    </div>`
+    
+              
+    
+}
+
+
+  function notification(_text) {
+    document.querySelector(".alert").style.display = "block"
+    document.querySelector("#notification").textContent = _text
+  }
+  
+  function notificationOff() {
+    document.querySelector(".alert").style.display = "none"
+  }
+
+  async function isWalletConnected(){
+    if(window.ethereum){
+      web3 = new Web3(window.ethereum);
+      setUpContracts()
+
+      const accounts = await web3.eth.getAccounts()
+      if(!accounts){
+        // document.getElementById("disconnectButton").style.display="none";
+        document.getElementById("connectButton").style.display="none";
+        walletConnected = false;
+      }else{
+        // document.getElementById("disconnectButton").style.display="block";
+        document.getElementById("connectButton").style.display="none";
+        defaultAccount = accounts[0];    
+
+        walletConnected = true;
+        await getDAOTokenBalance();
+      }
+
+
+    }
+    
+    
+  }
+
+  window.addEventListener('load', async () => {
+    notification("⌛ Loading...")
+    console.log("loading");
+
+    await isWalletConnected();
+
+    // await connectMetamaskWallet()
+
+    // await getBalance()
+    await renderPosts()
+    console.log(posts)
+    notificationOff()
+    setButtonClicks()
+
+  });
+
+  function setUpContracts(){
+    contract = new web3.eth.Contract(contentCuratorDAOAbi, contentCuratorDAOAddress);
+    erc20Contract = new web3.eth.Contract(erc20Abi, contentCuratorDAOTokenAddress)
+  }
+
+    /*  Connect Button */
+  document.querySelector("#connectButton").addEventListener("click", async (e) => {
+        const index = e.target.id
+        try{
+          await connectMetamaskWallet()
+          walletConnected = true;
+          // document.getElementById("disconnectButton").style.display="block";
+          document.getElementById("connectButton").style.display="none";
+
+          
+        } catch (error) {
+          notification(`⚠️ ${error.message}.`)
+          walletConnected = false;
+        }
+  })
+
+   /*  Claim Button */
+   document.querySelector("#claimButton").addEventListener("click", async (e) => {
+    const index = e.target.id
+    try{
+      const result = await contract.methods
+          .mintDAOToken()
+          .send({ from: defaultAccount })
+      await connectMetamaskWallet()
+      document.getElementById("claimButton").style.display="none";
+
+      
+    } catch (error) {
+      notification(`⚠️ ${error.message}.`)
+    }
+})
+
+
+   
+
+    /*  Disconnect Button */
+    document.querySelector("#disconnectButton").addEventListener("click", async (e) => {
+      const index = e.target.id
+      try{
+        await disconnectMetamaskWallet()
+        walletConnected = false;
+
+        // document.getElementById("disconnectButton").style.display="none";
+        document.getElementById("connectButton").style.display="block";
+        
+      } catch (error) {
+        notification(`⚠️ ${error.message}.`)
+        walletConnected = true;
+      }
+})
+
+
+
+ 
+  
+  /* Approve token spender */
+  async function approve(_price) {
+    const cUSDContract = new web3.eth.Contract(erc20Abi, cUSDTokenAddress)
+    console.log("toApprove")
+  
+    const result = await cUSDContract.methods
+      .approve(MPContractAddress, _price)
+      .send({ from: defaultAccount })
+      console.log(result)
+    return result
+  }
+
+  /* Vote  Button */
+  $(document).on( 'click', function () { 
+    alert('clicked');
+    });
+function setButtonClicks(){
+
+  document.querySelector("#marketplace").addEventListener("click", async (e) => {
+    e.preventDefault();
+    console.log('click')
+    if (e.target.className.includes("voteBtn")) {
+      console.log('let the voting begin')
+      const index = e.target.id
+      const ipfsURL = posts[index].ipfsURL
+      const owner = posts[index].owner
+      console.log(index, ipfsURL, owner)
+
+      try{
+        let walletBalance = await getDAOTokenBalance()
+        console.log(walletBalance)
+        if (walletBalance<properties[index].price/properties[index].numShares){
+          notification(`You do not have enough CCD to vote`)
+          return
+        }
+      } catch (error) {
+        notification(`⚠️ ${error.message}.`)
+      }
+      
+
+
+      // notification("⌛ Waiting for payment approval...")
+      // try {
+      //   await approve(properties[index].price)
+      
+      // } catch (error) {
+      //   notification(`⚠️ ${error}.`)
+      // }
+      notification(`⌛ Awaiting vote ...`)
+    try {
+      const result = await contract.methods
+        .vote(ipfsURL, owner)
+        .send({ from: defaultAccount })
+        console.log(result);
+      notification(`🎉 You successfully voted "${posts[index].description}".`)
+      await renderPosts()
+      getDAOTokenBalance()
+    } catch (error) {
+      notification(`⚠️ ${error.message}.`)
+    }
+  }
+})
+}
+
+/* Cancel Sale */
+document.querySelector("#marketplace").addEventListener("click", async (e) => {
+  if (e.target.className.includes("cancelSaleBtn")) {
+    console.log('cancelling sale')
+    const index = e.target.id
+    try{
+      let canBeCancelled =  await contract.methods.canBeCancelled(index).call()
+      
+      if (canBeCancelled){
+        notification(`canelling Sale`)
+
+        const result = await contract.methods
+          .cancelPropertySale(index)
+          .send({ from: defaultAccount })
+        notification(`🎉 You successfully cancelled "${properties[index].name}".`)
+        getProperties()
+        getBalance()
+        return
+      }else{
+        notification(`You cannot cancel this sale.`)
+
+      }
+    } catch (error) {
+      notification(`⚠️ ${error.message}.`)
+    } 
+}
+})
+
+
+
